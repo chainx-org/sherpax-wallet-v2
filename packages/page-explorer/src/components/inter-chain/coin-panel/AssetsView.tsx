6f4f44895cdceaaebd0685240001dcd1e142640e
GetWebHB <1597834867@qq.com>
2022-07-13 16:38:30 +0800

    fix: decimal dollar

--- packages/page-explorer/src/components/inter-chain/coin-panel/AssetsView.tsx
+++ packages/page-explorer/src/components/inter-chain/coin-panel/AssetsView.tsx
@@ -13,6 +13,7 @@ const AssetsView = (props: Props) => {
   const { currentAccount } = useContext(AccountContext);
   let { btcDollar, coinExchangeRate } = useContext(CoinPriceContext);
   let [sBTCObj] = coinExchangeRate.filter((item: any) => item.coin === 'sBTC');
+  const price = sBTCObj?.price ? sBTCObj.price : 0
 
 
   const sbtcAssets = useSbtcAssets(currentAccount);
@@ -21,15 +22,15 @@ const AssetsView = (props: Props) => {
     <div className='assets-view'>
       <div className='balance'>
         <p>Balance</p>
-        <h2 className='balance-tit'>{sbtcAssets.balance} sBTC <span className='dollar'> (≈ ${Number(sBTCObj?.price * sbtcAssets.balance).toFixed(2)}) </span> </h2>
+        <h2 className='balance-tit'>{sbtcAssets.balance} sBTC <span className='dollar'> (≈ ${Number(price * sbtcAssets.balance).toFixed(2)}) </span> </h2>
       </div>
       <div className='transferable'>
         <p>Transferable</p>
-        <h2>{(sbtcAssets.balance - sbtcAssets.reserved).toFixed(4)} sBTC <span className='dollar small-px'> (≈ ${Number(sBTCObj?.price * sbtcAssets.reserved).toFixed(2)}) </span> </h2>
+        <h2>{(sbtcAssets.balance - sbtcAssets.reserved).toFixed(4)} sBTC <span className='dollar small-px'> (≈ ${Number(price * sbtcAssets.reserved).toFixed(2)}) </span> </h2>
       </div>
       <div className='reserved'>
         <p>Withdrawal Reserved</p>
-        <h2>{sbtcAssets.reserved} sBTC <span className='dollar small-px'> (≈ ${Number(sBTCObj?.price * sbtcAssets.reserved).toFixed(2)}) </span> </h2>
+        <h2>{sbtcAssets.reserved} sBTC <span className='dollar small-px'> (≈ ${Number(price * sbtcAssets.reserved).toFixed(2)}) </span> </h2>
       </div>
     </div>
   );
