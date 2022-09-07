8d4a35d339f79401056f4fa86ee9881f15e77fe9
GetWebHB <1597834867@qq.com>
2022-09-07 15:13:22 +0800

    feat: apk swap

--- packages/apps/src/Web3Library.ts
+++ packages/apps/src/Web3Library.ts
@@ -4,9 +4,9 @@ import {Web3Provider} from '@ethersproject/providers'
 import {InjectedConnector} from '@web3-react/injected-connector'
 import {WalletConnectConnector} from '@web3-react/walletconnect-connector'
 
-export const POLLING_INTERVAL = 12000
+export const POLLING_INTERVAL = 15000
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 56, 88, 89, 97, 137, 1337, 80001],
+  supportedChainIds: [1, 3, 4, 5, 42, 56, 88, 89, 97, 137, 1337, 1506, 80001],
 })
 
 const RPC_URLS = {
